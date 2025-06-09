fb577bd905527a21c09d729c94772885e714dbd8
gtdev <gtdevrandom@gmail.com>
2025-06-09 09:37:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 const CACHE_NAME = 'programme-hebdo-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
+  '/hebdo-app/',
+  '/hebdo-app/index.html',
+  '/hebdo-app/manifest.json',
+  '/hebdo-app/icons/icon-192.png',
+  '/hebdo-app/icons/icon-512.png',
+  '/hebdo-app/favicon.ico'
 ];
 
-// Cache les assets à l'installation
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -14,11 +16,21 @@ self.addEventListener('install', event => {
   );
 });
 
-// Répond avec cache ou réseau
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
